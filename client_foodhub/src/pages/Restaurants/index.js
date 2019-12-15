a81e57b0de7b74dc16f2b7efee6533fb43fb52be
Oleksander Kozyriev <oleksander.kozyriev@gmail.com>
2019-12-15 17:51:59 +0200

    Color improvements to Restaurants Page

--- client_foodhub/src/pages/Restaurants/index.js
+++ client_foodhub/src/pages/Restaurants/index.js
@@ -11,6 +11,7 @@ const useStyles = makeStyles(theme => ({
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
+        background: '#b2fab4',
     },
 }));
 
